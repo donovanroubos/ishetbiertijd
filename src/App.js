5b637187da47b69c30d72820c6e718b43dbee8d5
Lorenzo <29688706+lorenzodelijser@users.noreply.github.com>
2021-03-30 23:42:59 +0200

    Added Kingsday as isBeerTime

--- src/App.js
+++ src/App.js
@@ -7,9 +7,18 @@ function App() {
   const dayName = days[new Date().getDay()];
 
   const correctDay = dayName === 'Friday' || dayName === 'Saturday' || dayName === 'Sunday'
-  
-  const isBeerTime = hour >= 16 && hour >= 0 && correctDay
-  
+  const currentYear = new Date().getFullYear();
+  const kingsDay = new Date(`${currentYear}-04-27`)
+
+  function sameDay(d1, d2) {
+    return d1.getFullYear() === d2.getFullYear() &&
+      d1.getMonth() === d2.getMonth() &&
+      d1.getDate() === d2.getDate();
+  }
+
+  const isBeerTime = (hour >= 16 && hour >= 0 && correctDay) || (sameDay(currentDate, kingsDay))
+
+
   return (
     <div className="App">
       <header className="App-header">
